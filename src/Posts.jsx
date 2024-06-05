2a489abe532bf2e076c029849ff54cc2cc20d47a
dmcclellan3 <wdmcclellan3@gmail.com>
2024-06-05 16:57:47 -0400

    saving

--- src/Posts.jsx
+++ src/Posts.jsx
@@ -4,7 +4,9 @@ import { AuthContext } from './authContext';
 
 const Posts = () => {
     const [posts, setPosts] = useState([])
+    const [newPost, setNewPost] = useState('')
     const { auth } = useContext(AuthContext)
+
     useEffect (() => {
        getPosts ({ auth })
         .then(response => {
@@ -13,14 +15,42 @@ const Posts = () => {
         })
     }, []);
 
+    const handleNewPost = (e) => {
+        setNewPost(e.target.value);
+    };
+    
+    const handlePostSubmit = async () => {
+        e.preventDefault();
+        try {
+            const response = await handlePostSubmit({ auth, content: newPost });
+            console.log('CREATE POST RESPONSE: ', response);
+            setPosts([response.data, ...posts]); 
+            setNewPost(''); 
+        } catch (error) {
+            console.error('Error creating post:', error);
+        }
+    };
+    
+
     return (
         <div>
             <h1>Posts</h1>
+            <form onClick={handlePostSubmit}>
+                <input
+                type='text'
+                value={newPost}
+                onChange={handleNewPost} 
+                placeholder="What's on your mind?"
+                />
+                <br />
+                <br />
+                <button type='submit'>Post</button>
+            </form>
             {posts.map(post => (
-                <div key={post.id}>
-                    <h2>{post.user.username}</h2>
-                    <p>{post.content}</p>
-                </div>
+            <div key={post.id}>
+                <h2>{post.user.username}</h2>
+                <p>{post.content}</p>
+            </div>
             ))} 
         </div>
     );
